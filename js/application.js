ef80faf363aaef01929c20151696434c9926326a
Jonasz Karwacki <jonaszakr@gmail.com>
2018-12-07 00:47:44 -0800

    modularize height adjust mention method

--- js/application.js
+++ js/application.js
@@ -18,10 +18,8 @@
 // })();
 
 $(window).on('load', function() {
+  adjustContentHeight()
 
-  if($('.content').height() < $(window).height()) {
-    $('.content').css({'top': $(window).height()/2-$('.content').height()/2 + 'px'})
-  }
   var mX, mY, distance, $element  = $('.sidebar a');
   var elements = [$element].concat($('.rainbow'))
   function calculateDistance(elem, mouseX, mouseY) {
@@ -33,9 +31,19 @@ $(window).on('load', function() {
       mY = e.pageY;
       for(var i=0; i<elements.length; i++) {
         distance = calculateDistance(elements[i], mX, mY);
-        elements[i] = elements[i].css('font-variation-settings', '"wght"'+(800-distance))
+        elements[i].css('font-variation-settings', '"wght"'+(800-distance))
         console.log(distance)
       }
       //$distance.text(distance);
   });
 });
+
+$( window ).resize(function() {
+  adjustContentHeight()
+});
+
+function adjustContentHeight() {
+  if($('.content').height() < $(window).height()) {
+    $('.content').css({'top': $(window).height()/2-$('.content').height()/2 + 'px'})
+  }
+}
