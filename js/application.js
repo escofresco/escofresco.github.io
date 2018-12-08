18ccfdd1cece0d46e4c654b879f62cf4fafe4997
Jonasz Karwacki <jonaszakr@gmail.com>
2018-12-07 20:31:19 -0800

    Fix post loading issues

--- js/application.js
+++ js/application.js
@@ -18,10 +18,12 @@
 // })();
 
 $(window).on('load', function() {
-  adjustContentHeight()
+  adjustContentHeight();
+  adjustSidebarHeight();
 
   var mX, mY, distance, $element  = $('.sidebar a');
-  var elements = [$element].concat($('.rainbow'))
+  // var elements = [$element].concat($('.rainbow'));
+  var classes = ['.sidebar a', '.rainbow', '.content a'] //.concat($('.content a'));
   function calculateDistance(elem, mouseX, mouseY) {
       return Math.floor(Math.sqrt(Math.pow(mouseX - (elem.offset().left+(elem.width()/2)), 2) + Math.pow(mouseY - (elem.offset().top+(elem.height()/2)), 2)));
   }
@@ -29,10 +31,14 @@ $(window).on('load', function() {
   $(document).mousemove(function(e) {
       mX = e.pageX;
       mY = e.pageY;
-      for(var i=0; i<elements.length; i++) {
-        distance = calculateDistance(elements[i], mX, mY);
-        elements[i].css('font-variation-settings', '"wght"'+(800-distance))
-        console.log(distance)
+      for(var i=0; i<classes.length; i++) {
+        $(classes[i]).each(function(j, elm) {
+
+          if ($(elm).length) {
+            distance = calculateDistance($(elm), mX, mY);
+            $(elm).css('font-variation-settings', '"wght"'+(800-distance));
+          }
+        });
       }
       //$distance.text(distance);
   });
@@ -45,5 +51,11 @@ $( window ).resize(function() {
 function adjustContentHeight() {
   if($('.content').height() < $(window).height()) {
     $('.content').css({'top': $(window).height()/2-$('.content').height()/2 + 'px'})
+  }
+}
+
+function adjustSidebarHeight() {
+  if($('.sidebar').height() < $(window).height()) {
+    $('.avatar').css({'top': $(window).height()/2-$('.sidebar').height()/2 + 'px'})
   }
 }
