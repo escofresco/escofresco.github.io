aa7c29315d9ca88ca24bcb295cfdf970c8aa5f08
Jonasz Karwacki <jonaszakr@gmail.com>
2020-01-17 07:32:41 -0800

    comment out dynamic font

--- js/application.js
+++ js/application.js
@@ -4,31 +4,31 @@ $(window).on('load', function() {
   adjustContentHeight();
   adjustSidebarHeight();
 
-  var mX, mY, distance, $element  = $('.sidebar a');
-  // var elements = [$element].concat($('.rainbow'));
-  var classes = ['.sidebar a', '.rainbow'] //.concat($('.content a'));
-  function calculateDistance(elem, mouseX, mouseY) {
-      return Math.floor(Math.sqrt(Math.pow(mouseX - (elem.offset().left+(elem.width()/2)), 2) + Math.pow(mouseY - (elem.offset().top+(elem.height()/2)), 2)));
-  }
-
-  $(document).mousemove(function(e) {
-      mX = e.pageX;
-      mY = e.pageY;
-      for(var i=0; i<classes.length; i++) {
-        $(classes[i]).each(function(j, elm) {
-
-          if ($(elm).length) {
-            distance = calculateDistance($(elm), mX, mY);
-            $(elm).css('font-variation-settings', '"wght"'+(800-distance));
-          }
-        });
-      }
-      //$distance.text(distance);
-  });
+  // var mX, mY, distance, $element  = $('.sidebar a');
+  // // var elements = [$element].concat($('.rainbow'));
+  // var classes = ['.sidebar a', '.rainbow'] //.concat($('.content a'));
+  // function calculateDistance(elem, mouseX, mouseY) {
+  //     return Math.floor(Math.sqrt(Math.pow(mouseX - (elem.offset().left+(elem.width()/2)), 2) + Math.pow(mouseY - (elem.offset().top+(elem.height()/2)), 2)));
+  // }
+  //
+  // $(document).mousemove(function(e) {
+  //     mX = e.pageX;
+  //     mY = e.pageY;
+  //     for(var i=0; i<classes.length; i++) {
+  //       $(classes[i]).each(function(j, elm) {
+  //
+  //         if ($(elm).length) {
+  //           distance = calculateDistance($(elm), mX, mY);
+  //           $(elm).css('font-variation-settings', '"wght"'+(800-distance));
+  //         }
+  //       });
+  //     }
+  // });
 });
 
 $( window ).resize(function() {
-  adjustContentHeight()
+  adjustContentHeight();
+  adjustSidebarHeight();
 });
 
 function adjustContentHeight() {
@@ -39,6 +39,6 @@ function adjustContentHeight() {
 
 function adjustSidebarHeight() {
   if($('.sidebar').height() < $(window).height()) {
-    $('.avatar').css({'top': $(window).height()/2-$('.sidebar').height()/2 + 'px'})
+    $('.sidebar').css({'top': $(window).height()/2-$('.sidebar').height()/2 + 'px'})
   }
 }
