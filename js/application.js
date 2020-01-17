d60b35a7f0834745a90bcab0bc29ecb2c99cd351
Jonasz Karwacki <jonaszakr@gmail.com>
2020-01-17 06:27:57 -0800

    remove comment

--- js/application.js
+++ js/application.js
@@ -1,21 +1,4 @@
 // // Your JavaScript
-// (function() {
-//
-//     var mX, mY, distance, $element  = $('.sidebar a');
-//
-//     function calculateDistance(elem, mouseX, mouseY) {
-//         return Math.floor(Math.sqrt(Math.pow(mouseX - (elem.offset().left+(elem.width()/2)), 2) + Math.pow(mouseY - (elem.offset().top+(elem.height()/2)), 2)));
-//     }
-//
-//     $(document).mousemove(function(e) {
-//         mX = e.pageX;
-//         mY = e.pageY;
-//         console.log($element.offset())
-//         distance = calculateDistance($element, mX, mY);
-//         console.log(distance)
-//         //$distance.text(distance);
-//     });
-// })();
 
 $(window).on('load', function() {
   adjustContentHeight();
